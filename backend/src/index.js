0678cd337cf7d0b0a71ab3b55534d8f64ab8a32d
sabal62 <sabalplayz.new@gmail.com>
2025-10-25 15:46:00 +0545

    We-Talk by we

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,9 +32,10 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
 
-    app.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    app.get("/:path([^]*)", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
+
 
 }
 
