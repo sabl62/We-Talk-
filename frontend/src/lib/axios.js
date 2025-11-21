3ab6cd78b57f07cdb0dde805510cbae1bc1c01f8
sabal62 <sabalplayz.new@gmail.com>
2025-11-21 22:03:20 +0545

    Hosted.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api": "/api",
+    baseURL: import.meta.env.MODE === "development" ? "https://we-talk-1.onrender.com": "/api",
     withCredentials: true,
 })
